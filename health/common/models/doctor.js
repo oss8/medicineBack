646f84083a9db656b041891487e533ee5f23a542
summerge <summerge@summergedeMacBook-Pro.local>
2017-05-02 17:45:27 +0800

    chang code

--- health/common/models/doctor.js
+++ health/common/models/doctor.js
@@ -153,10 +153,10 @@ module.exports = function (Doctor) {
             var _uuid = "";
             if (_patientInfo.Result.length == 0) {
                 _uuid = uuid.v4();
-                bsSQL = "INSERT INTO hh_publicUser (id, mobile, cardNo, medicalNo, status, name, type, sex, casetype, province, city, region, address) VALUES ('" + _uuid + "', '" + AddPatient.pmobile + "', '" + AddPatient.pcardno + "','"+AddPatient.pmedicalNo+"',  0,  '" + AddPatient.pname + "', 0, " + AddPatient.psex + ", '" + AddPatient.casetype + "', '" + AddPatient.province + "', '" + AddPatient.city + "', '" + AddPatient.region + "', '" + AddPatient.address + "');";
+                bsSQL = "INSERT INTO hh_publicUser (id, mobile, cardNo, medicalNo, status, name, type, sex, casetype, province, city, region, address, birthday) VALUES ('" + _uuid + "', '" + AddPatient.pmobile + "', '" + AddPatient.pcardno + "','"+AddPatient.pmedicalNo+"',  0,  '" + AddPatient.pname + "', 0, " + AddPatient.psex + ", '" + AddPatient.casetype + "', '" + AddPatient.province + "', '" + AddPatient.city + "', '" + AddPatient.region + "', '" + AddPatient.address + "','"+ AddPatient.birthday+"');";
             } else {
                 _uuid = _patientInfo.Result[0].id;
-                bsSQL = "update hh_publicuser set name = '" + AddPatient.pname + "', cardNo = '" + AddPatient.pcardno + "',sex ='" + AddPatient.psex + "',casetype = '" + AddPatient.casetype + "',province='" + AddPatient.province + "',city='" + AddPatient.city + "',region='" + AddPatient.region + "',address='" + AddPatient.address + "',mobile = '" + AddPatient.pmobile + "' where id = '"+ _uuid +"';";
+                bsSQL = "update hh_publicuser set name = '" + AddPatient.pname + "', cardNo = '" + AddPatient.pcardno + "',sex ='" + AddPatient.psex + "',casetype = '" + AddPatient.casetype + "',province='" + AddPatient.province + "',city='" + AddPatient.city + "',region='" + AddPatient.region + "',address='" + AddPatient.address + "',mobile = '" + AddPatient.pmobile + "',birthday = '"+ AddPatient.birthday +"' where id = '"+ _uuid +"';";
             }
 
 
@@ -182,7 +182,7 @@ module.exports = function (Doctor) {
         {
             http: { verb: 'post' },
             description: '添加病人',
-            accepts: { arg: 'AddPatient', type: 'object', description: '{"patientid":"","doctorid":"123","pmobile":"13857194279","pcardno":"","pmedicalNo":"","pname":"","psex":"0","province":"","city":"","region":"","address":"","casetype":"0"}' },
+            accepts: { arg: 'AddPatient', type: 'object', description: '{"patientid":"","doctorid":"123","pmobile":"13857194279","pcardno":"","pmedicalNo":"","pname":"","psex":"0","province":"","city":"","region":"","address":"","casetype":"0","birthday":""}' },
             returns: { arg: 'AddPatient', type: 'object', root: true }
         }
     );
