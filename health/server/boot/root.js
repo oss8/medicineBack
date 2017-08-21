8c07c850fb95894e59bbc67c52aa0cfd8fccef62
ling <geling1974@gmail.com>
2017-08-21 13:27:16 +0800

    chang code

--- health/server/boot/root.js
+++ health/server/boot/root.js
@@ -5,4 +5,44 @@ module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
+
+  var _ = require('underscore');
+  
+  
+    var TokenList = ['RequestUserInfo',
+  'ModifyUserInfo',
+  'RequestMyQRCode',
+  'ModifyFollowInfo',
+  'removeFollow',
+  'RequestUserMonitor'];
+  
+  
+    router.use(function (req, res, next) {
+      var f_name = req.path.substr(req.path.lastIndexOf('/') + 1);
+  
+      var find = _.find(TokenList, function (item) {
+        return item.toUpperCase() == f_name.toUpperCase();
+      })
+  
+      if (!_.isEmpty(find)) {
+        if (req.headers.token == undefined) {
+          res.writeHead(401);
+          res.end();
+        }
+        else {
+          var OpenID = {};
+          try {
+            OpenID = GetOpenIDFromToken(req.headers.token);
+            next();
+          } catch (err) {
+            res.writeHead(403);
+            res.end();
+          }
+        }
+      }
+      else{
+        next();
+      }
+    })
+
 };
