2d721db7bd9f40f850bf8c44d6f64c4800760ff7
ling <geling1974@gmail.com>
2018-02-10 21:31:03 +0800

    chang code

--- health/server/server.js
+++ health/server/server.js
@@ -180,7 +180,7 @@ net.createServer(function(sock) {
 
         if ( RecvData.indexOf('NBES') == 0){
             //var _out = new Buffer(Str2Bytes(RecvData));
-            sock.write(new Buffer(RecvData));
+            sock.write(new Buffer(data));
 
             var find = _.find(socketList, function(item){
                 return item.remoteAddress == sock.remoteAddress && item.remotePort == sock.remotePort;
